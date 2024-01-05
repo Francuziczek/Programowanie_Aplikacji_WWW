60b352fd8b2f8cd6b97018cb284458ffeeba5038
Francuziczek <92163538+Francuziczek@users.noreply.github.com>
2024-01-05 18:58:24 +0100

    Add files via upload

---
+++ LAB_11/showpage.php
@@ -0,0 +1,18 @@
+<?php
+	function PokazPodstrone($id) {
+		$id_clear = htmlspecialchars($id);
+		$mysqli = new mysqli("localhost", "root", "", "moja_strona");
+		
+		$query = "SELECT * FROM page_list WHERE id='$id_clear' LIMIT 1";
+		$result = mysqli_query($mysqli, $query);
+		$row = mysqli_fetch_array($result);
+		
+		if(empty($row['id'])) {
+			$web = '[nie_znaleziono_strony]';
+		}
+		else {
+			$web = $row['page_content'];
+		}
+		return $web;
+	}
+?>
