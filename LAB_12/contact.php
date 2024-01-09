106695e7649eda727f5ef32a3ed3a862ca783fdd
Francuziczek <92163538+Francuziczek@users.noreply.github.com>
2024-01-09 20:22:42 +0100

    Add files via upload

--- LAB_12/contact.php
+++ LAB_12/contact.php
@@ -1,51 +1,93 @@
 <body style="background-color: grey;>
 <?php
 echo '<link rel="stylesheet" href="./css/style.css">';
-function PokazKontakt()
-    {
-        echo '
-            <h1>Kontakt</h1>
-            <form action="contact.php?action=wyslij_mail" method="post">
-                <p>Temat:</p>
-                <input type="text" name="temat"><br>
-                <p>Tresc:</p>
-                <textarea name="tresc" rows="4" cols="25"></textarea><br>
-                <p>Email:</p>
-                <input type="email" name="email"><br>
-                <input type="submit" value="Wyślij">
-            </form>';
-    }
 
-function WyslijMailKontakt($odbiorca)
-    {
-        if (empty($_POST['temat']) || empty($_POST['tresc']) || empty($_POST['email'])) {
-            echo '</br>[Nie wszystkie pola zostaly wypelnione]';
-            echo PokazKontakt();
-        } else {
-            $mail['subject'] = $_POST['temat'];
-            $mail['body'] = $_POST['tresc'];
-            $mail['sender'] = $_POST['email'];
-            $mail['reciptient'] = $odbiorca;
-
-            $header = "From: Formularz kontaktowy <" . $mail['sender'] . ">\n";
-            $header .= "MIME-Version: 1.0\nContent-Type: text/plain; charset=utf-8\nContent-Transfer-Encoding: \n";
-            $header .= "X-Sender: <" . $mail['sender'] . ">\n";
-            $header .= "X-Mailer: PRapWWW mail 1.2\n";
-            $header .= "X-Priority: 3\n";
-            $header .= "Return-Path: <" . $mail['sender'] . ">\n";
-
-            mail($mail['reciptient'], $mail['subject'], $mail['body'], $header);
-
-            echo 'Wiadomość została wysłana';
-        }
-    }
-
-
-if (isset($_GET['action']) && $_GET['action'] == 'wyslij_mail') {
-    $odbiorca = 'domel2002@o2.pl';
-    WyslijMailKontakt($odbiorca);
-} else {
-    PokazKontakt();
+function PokazKontakt() {
+    $wynik = '<div>
+			<h1>KONTAKT:</h1><br>
+			<form action="" method="post"
+			<label for="temat">Temat:</label>
+			<input type="text" name="temat" id="temat" required><br>
+			<label for="email">Email:</label>
+			<input type="text" name="email" id="email" required><br>
+			<label for="tresc">Treść:</label>
+			<input type="text" name="tresc" id="tresc" required><br>
+			<input type="submit" name="wyslijwiadomosc" value="Wyślij">
+		</form>
+		</div>';
+	return $wynik;
 }
+
+function WyslijMailKontakt($odbiorca) {
+    if (empty($_POST['temat']) || empty($_POST['tresc']) || empty($_POST['email'])) {
+        echo '</br>Nie wszystkie pola zostały wypełnione';
+        echo PokazKontakt();
+    } else {
+        $mail['subject'] = $_POST['temat'];
+        $mail['body'] = $_POST['tresc'];
+        $mail['sender'] = $_POST['email'];
+        $mail['recipient'] = $odbiorca;
+
+        $header = "From: Formularz kontaktowy <" . $mail['sender'] . ">\n";
+        $header .= "MIME-Version: 1.0\nContent-Type: text/plain; charset=utf-8\nContent-Transfer-Encoding: \n";
+        $header .= "X-Sender: <" . $mail['sender'] . ">\n";
+        $header .= "X-Mailer: PRapWWW mail 1.2\n";
+        $header .= "X-Priority: 3\n";
+        $header .= "Return-Path: <" . $mail['sender'] . ">\n";
+
+        mail($mail['recipient'], $mail['subject'], $mail['body'], $header);
+
+        echo 'Wiadomość została wysłana';
+    }
+}
+
+function PokazPrzypomnijHaslo() {
+	echo '<h1>Przypomnij haslo:</h1><br>';
+    $wynik = '<form action="" method="post">
+			<label for="email_pass">Email:</label>
+			<input type="email_pass" name="email_pass" required>
+			<input type="submit" name="wyslijhaslo" value="Przypomnij hasło">
+        </form>';
+	return $wynik;
+}
+
+function PrzypomnijHaslo($odbiorca) {
+
+    if (empty($_POST['email_pass'])) {
+        echo '</br>Nie podano adresu email';
+        echo PokazPrzypomnijHaslo();
+    } else {
+		$mail['subject'] = "Wiadomość o przypomnienie hasła";
+        $mail['body'] = "Przypomnienie hasła";
+        $mail['sender'] = $_POST['email_pass'];
+        $mail['reciptient'] = $odbiorca;
+
+        $header = "From: Formularz kontaktowy <" . $mail['sender'] . ">\n";
+        $header .= "MIME-Version: 1.0\nContent-Type: text/plain; charset=utf-8\nContent-Transfer-Encoding: \n";
+        $header .= "X-Sender: <" . $mail['sender'] . ">\n";
+        $header .= "X-Mailer: PRapWWW mail 1.2\n";
+        $header .= "X-Priority: 3\n";
+        $header .= "Return-Path: <" . $mail['sender'] . ">\n";
+
+        mail($mail['reciptient'], $mail['subject'], $mail['body'], $header);
+
+        echo 'Wiadomość o przypomnienie hasła została wysłana';
+		echo '<a href="lab1.php">Strona Główna</a>';
+	}
+}
+
+
+if (isset($_POST['wyslijhaslo'])) {
+    PrzypomnijHaslo('164367@student.uwm.edu.pl');
+} elseif (isset($_POST['wyslijwiadomosc'])) {
+    WyslijMailKontakt('164367@student.uwm.edu.pl');
+} else {
+    echo PokazKontakt();
+    echo PokazPrzypomnijHaslo();
+}
+
 ?>
 </body>
+<div style="item-align: top; position: fixed; bottom : 1px; right: 10px; padding: 10px;">
+<a href="lab1.php">STRONA GŁÓWNA</a>
+</div>
